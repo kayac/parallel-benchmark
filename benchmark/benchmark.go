52723aa81476a8edc45c1a58ac0ab53cc363b850
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2014-07-10 23:48:56 +0900

    Initial commit

---
+++ benchmark/benchmark.go
@@ -0,0 +1,132 @@
+package benchmark
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var Debug = false
+
+var TrapSignals = []os.Signal{
+	syscall.SIGHUP,
+	syscall.SIGINT,
+	syscall.SIGTERM,
+	syscall.SIGQUIT}
+
+type Runner struct {
+	Setup       func(*Worker)
+	Teardown    func(*Worker)
+	Benchmark   func(*Worker) int
+	Duration    time.Duration
+	Concurrency int
+}
+
+type Worker struct {
+	Id int
+	Stash  map[string]interface{}
+}
+
+type Result struct {
+	Score   int
+	Elapsed time.Duration
+}
+
+func debug (s string, v ...interface{}) {
+	if Debug {
+		log.Printf(s, v...)
+	}
+}
+
+func (r *Runner) Run() *Result {
+	c := r.Concurrency
+	log.Printf("starting benchmark: concurrency: %d, time: %s, GOMAXPROCS: %d", c, r.Duration, runtime.GOMAXPROCS(0))
+
+	startCh := make(chan bool, c)
+	readyCh := make(chan bool, c)
+	stopCh := make(chan bool, c)
+	scoreCh := make(chan int, c)
+	var wg sync.WaitGroup
+
+	// spawn worker goroutines
+	for i := 0; i < c; i++ {
+		debug("spwan worker[%d]", i);
+		go func(n int) {
+			wg.Add(1)
+			defer wg.Done()
+			score := 0
+			worker := &Worker{
+				Id: n,
+				Stash: make(map[string]interface{}),
+			}
+			if r.Setup != nil {
+				debug("worker[%d] Setup()", n)
+				r.Setup(worker)
+			}
+			readyCh <- true // ready of worker:n
+			<-startCh       // notified go benchmark from Runner
+			debug("worker[%d] starting Benchmark()", n);
+		BENCH:
+			for {
+				select {
+				case <-stopCh:
+					scoreCh <- score
+					break BENCH
+				default:
+					score += r.Benchmark(worker)
+				}
+			}
+			debug("worker[%d] done Benchmark() score: %d", n, score);
+			if r.Teardown != nil {
+				debug("worker[%d] Teardown()", n)
+				r.Teardown(worker)
+			}
+			debug("worker[%d] exit", n)
+		}(i)
+	}
+
+	// wait for ready of workres
+	debug("waiting for all workers finish Setup()");
+	for i := 0; i < c; i++ {
+		<-readyCh
+	}
+
+	// notify "start" to workers
+	close(startCh)
+	start := time.Now()
+
+	// wait for catching signal or timed out
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, TrapSignals...)
+	select {
+	case s := <-signalCh:
+		switch sig := s.(type) {
+		case syscall.Signal:
+			log.Printf("Got signal: %s(%d)", sig, sig)
+		default:
+			debug("timed out")
+			break
+		}
+	case <-time.After(r.Duration):
+		break
+	}
+
+	// notify "stop" to workers
+	close(stopCh)
+
+	// collect score from workers
+	totalScore := 0
+	for i := 0; i < c; i++ {
+		totalScore += <-scoreCh
+	}
+	end := time.Now()
+	elapsed := end.Sub(start)
+	log.Printf("done benchmark: score %d, elapsed %s = %f / sec\n", totalScore, elapsed, float64(totalScore) / float64(elapsed) * float64(time.Second))
+
+	wg.Wait()
+	return &Result{ Score: totalScore, Elapsed: elapsed }
+}
---
+++ examples/fib.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"time"
+	"os"
+	"strconv"
+	"github.com/fujiwara/parallel-benchmark/benchmark"
+)
+
+func main() {
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Panicf("invalid number", os.Args[1])
+	}
+	runner := &benchmark.Runner{
+		Teardown: func(w *benchmark.Worker) {
+			time.Sleep(time.Duration(1)*time.Second)
+		},
+		Benchmark: func(w *benchmark.Worker) int {
+			fib(n)
+			return 1
+		},
+		Duration: time.Duration(3)*time.Second,
+		Concurrency: 4,
+	}
+	runner.Run()
+}
+
+func fib(n int) int {
+	if n == 0 {
+		return 0;
+	}
+	if n == 1 {
+		return 1;
+	}
+	return (fib(n - 1) + fib(n - 2));
+}
