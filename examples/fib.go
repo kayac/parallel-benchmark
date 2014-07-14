685266eb7cffcbcc046256feff193f9a54e18827
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2014-07-14 12:55:54 +0900

    update examples

--- examples/fib.go
+++ examples/fib.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"github.com/fujiwara/parallel-benchmark/benchmark"
 	"log"
-	"time"
 	"os"
 	"strconv"
-	"github.com/fujiwara/parallel-benchmark/benchmark"
+	"time"
 )
 
 func main() {
@@ -13,23 +13,23 @@ func main() {
 	if err != nil {
 		log.Panicf("invalid number", os.Args[1])
 	}
-	runner := &benchmark.Runner{
-		Benchmark: func(w *benchmark.Worker) int {
+	result := benchmark.RunFunc(
+		func() (subscore int) {
 			fib(n)
 			return 1
 		},
-		Duration: time.Duration(3)*time.Second,
-		Concurrency: 4,
-	}
-	runner.Run()
+		time.Duration(3) * time.Second,
+		10,
+	)
+	log.Printf("%#v", result)
 }
 
 func fib(n int) int {
 	if n == 0 {
-		return 0;
+		return 0
 	}
 	if n == 1 {
-		return 1;
+		return 1
 	}
-	return (fib(n - 1) + fib(n - 2));
+	return (fib(n-1) + fib(n-2))
 }
---
+++ examples/httpbench.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"github.com/fujiwara/parallel-benchmark/benchmark"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type myWorker struct {
+	URL    string
+	client *http.Client
+}
+
+func (w *myWorker) Setup() {
+	w.client = &http.Client{}
+}
+
+func (w *myWorker) Teardown() {
+}
+
+func (w *myWorker) Process() (subscore int) {
+	resp, err := w.client.Get(w.URL)
+	if err == nil {
+		defer resp.Body.Close()
+		_, _ = ioutil.ReadAll(resp.Body)
+		if resp.StatusCode == 200 {
+			return 1
+		}
+	} else {
+		log.Printf("err: %v, resp: %#v", err, resp)
+	}
+	return 0
+}
+
+func main() {
+	var (
+		conn     int
+		duration int
+	)
+	flag.IntVar(&conn, "c", 1, "connections to keep open")
+	flag.IntVar(&duration, "d", 1, "duration of benchmark")
+	flag.Parse()
+	url := flag.Args()[0]
+	workers := make([]benchmark.Worker, conn)
+	for i, _ := range workers {
+		workers[i] = &myWorker{URL: url}
+	}
+	benchmark.Run(workers, time.Duration(duration)*time.Second)
+}
